6c5f7346f7a3bda10d053530e72b508a5f0757dc
bo.shao <bo.shao@aspentech.com>
2025-11-17 15:37:15 +0800

    console app

---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,7 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+var calculator = new Calculator.Calculator();
+int result = calculator.Add(3, 4);
+Console.WriteLine("Hello, World!");
+
+
